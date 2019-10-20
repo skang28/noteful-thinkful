9942f4028aeb5dc50fc3d520e658f7eb404c9788
Steven Kang <skang28@gmail.com>
2019-10-19 21:44:15 -0400

    updating context files

---
+++ src/Note/Note.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { format } from 'date-fns'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './Note.css'
+import NotesContext from '../NotesContext'
+import config from '../config'
+
+export default class Note extends React.Component {
+  static contextType = NotesContext
+  static defaultProps = {
+    onDeleteNote: () => {}
+  }
+
+  handleClickDelete = e => {
+    e.preventDefault()
+    const noteId = this.props.id
+
+    fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'appli'
+      }
+    })
+    .then(res => {
+      if(!res.ok)
+        return res.json().then(e => Promise.reject(e))
+      return res.json()
+    })
+    .then(() => {
+      this.context.deleteNote(noteId)
+      this.props.onDeleteNote(noteId)
+    })
+    .catch(error => {
+      console.error({error})
+    })
+  }
+
+  render() {
+    const {name, id, modified} = this.props
+    return (
+      <div className='Note'>
+        <h2 className='Note__title'>
+          <Link to={`/note/${id}`}>
+            {name}
+          </Link>
+        </h2>
+        <button className='Note__delete' type='button'>
+          <FontAwesomeIcon icon='trash-alt' />
+          {' '}
+          remove
+        </button>
+        <div className='Note__dates'>
+          <div className='Note__dates-modified'>
+            Modified
+            {' '}
+            <span className='Date'>
+              {format(modified, 'Do MMM YYYY')}
+            </span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/NoteListMain/NoteListMain.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Note from '../Note/Note'
+import CircleButton from '../CircleButton/CircleButton'
+import './NoteListMain.css'
+import NotesContext from '../NotesContext'
+import {getNotesForFolder} from '../notes-helpers'
+
+export default class NoteListMain extends React.Component {
+  static contextType = NotesContext
+  static defaultProps = {
+    match: {
+      params:{}
+    }
+  }
+
+  render() {
+    const {folderId} = this.props.match.params
+    const {notes=[]} = this.context
+    const notesForFolder = getNotesForFolder(notes, folderId)
+    
+    return (
+      <section className='NoteListMain'>
+        <ul>
+          {notesForFolder.map(note =>
+            <li key={note.id}>
+              <Note
+                id={note.id}
+                name={note.name}
+                modified={note.modified}
+              />
+            </li>
+          )}
+        </ul>
+        <div className='NoteListMain__button-container'>
+          <CircleButton
+            tag={Link}
+            to='/add-note'
+            type='button'
+            className='NoteListMain__add-note-button'
+          >
+            <FontAwesomeIcon icon='plus' />
+            <br />
+            Note
+          </CircleButton>
+        </div>
+      </section>
+    )
+  }
+}
