1b20495ef9fdeabfbf686985646766d0cea8d3e7
Steven Kang <skang28@gmail.com>
2019-12-17 15:30:28 -0500

    working version for deployment

--- src/NoteListNav/NoteListNav.js
+++ src/NoteListNav/NoteListNav.js
@@ -10,7 +10,7 @@ export default class NoteListNav extends React.Component {
   static contextType = NotesContext
 
   render() {
-    const {folders, notes} = this.context
+    const {folders=[], notes=[]} = this.context
     return (
       <div className='NoteListNav'>
         <ul className='NoteListNav__list'>
